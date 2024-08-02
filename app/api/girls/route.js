38f4c6c1f3d28cd6b1943ff3649bc4495f718b69
R0hit-KP <rohit.yallamelli@kraftpixel.com>
2024-08-02 22:49:52 +0530

    Boys and Girls routes fixed

--- app/api/girls/route.js
+++ app/api/girls/route.js
@@ -2,15 +2,15 @@ import { connectToDb } from "../../../lib/db";
 import User from "../../../models/User";
 import { NextResponse } from "next/server";
 
-// const setCorsHeaders = (res) => {
-//   res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
-//   res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-// };
+const setCorsHeaders = (res) => {
+  res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
+  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+};
 
 export async function GET(){
-  // const res = NextResponse.next(); 
-  // setCorsHeaders(res);
+  const res = NextResponse.next(); 
+  setCorsHeaders(res);
     try {
       await connectToDb();
       let boys = await User.find({gender:"Female"});
