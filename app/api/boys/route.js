5725596b1938811b0ffd6baf7d40dddca3b2b7ad
R0hit-KP <rohit.yallamelli@kraftpixel.com>
2024-08-03 00:09:25 +0530

    Fixing Delete,Boys and Girls Route part 3

--- app/api/boys/route.js
+++ app/api/boys/route.js
@@ -7,7 +7,10 @@ export async function GET(){
     try {
       await connectToDb();
       let boys = await User.find({gender:"Male"});
-      return NextResponse.json(boys,{status:200})
+      const res = NextResponse.json(boys,{status:200})
+      res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
+      res.headers.set('Access-Control-Allow-Methods', 'GET');
+      return res
     } catch (error) {
      console.log(error)
      return NextResponse.json({ error: 'An error occurred' }, { status: 500 })
