1401eb498e7bf6110737ef63a27ef22f06608b4a
R0hit-KP <rohit.yallamelli@kraftpixel.com>
2024-08-02 23:25:46 +0530

    Boys and Girls routes fixed again part 2

--- app/api/boys/route.js
+++ app/api/boys/route.js
@@ -1,20 +1,24 @@
 import { connectToDb } from "../../../lib/db";
 import User from "../../../models/User";
 import { NextResponse } from "next/server";
-const setCorsHeaders = (res) => {
-  res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
-  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-};
+// const setCorsHeaders = (res) => {
+//   res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
+//   res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+// };
 
 
 export async function GET(){
-  const res = NextResponse.next(); 
-  setCorsHeaders(res);
+  // const res = NextResponse.next(); 
+  // setCorsHeaders(res);
     try {
       await connectToDb();
       let boys = await User.find({gender:"Male"});
-      return NextResponse.json(boys,{status:200})
+     const res = NextResponse.json(boys,{status:200})
+     res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
+     res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+     res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+      return res
     } catch (error) {
      console.log(error)
      return NextResponse.json({ error: 'An error occurred' }, { status: 500 })
--- app/api/girls/route.js
+++ app/api/girls/route.js
@@ -2,19 +2,23 @@ import { connectToDb } from "../../../lib/db";
 import User from "../../../models/User";
 import { NextResponse } from "next/server";
 
-const setCorsHeaders = (res) => {
-  res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
-  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-};
+// const setCorsHeaders = (res) => {
+//   res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
+//   res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+// };
 
 export async function GET(){
-  const res = NextResponse.next(); 
-  setCorsHeaders(res);
+  // const res = NextResponse.next(); 
+  // setCorsHeaders(res);
     try {
       await connectToDb();
-      let boys = await User.find({gender:"Female"});
-      return NextResponse.json(boys,{status:200})
+      let girls = await User.find({gender:"Female"});
+      const res = NextResponse.json(girls,{status:200})
+      res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
+      res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+      return res
     } catch (error) {
      console.log(error)
      return NextResponse.json({ error: 'An error occurred' }, { status: 500 })
