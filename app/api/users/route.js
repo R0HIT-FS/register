7a1285d32ed1468eb6817ba487c6353916105bed
R0hit-KP <rohit.yallamelli@kraftpixel.com>
2024-12-01 18:28:36 +0530

    link changed in backend

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -4,27 +4,11 @@ import { NextResponse } from "next/server";
 
 
 const setCorsHeaders = (res) => {
-  res.headers.set('Access-Control-Allow-Origin', 'https://registration-kohl-ten.vercel.app'); // Your Vercel URL
+  res.headers.set('Access-Control-Allow-Origin', 'https://register-pink.vercel.app'); // Your Vercel URL
   res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
 };
 
-// const allowedOrigins = [
-//   'https://registration-kohl-ten.vercel.app', 
-//   'https://register-phi-silk.vercel.app'
-// ];
-
-// const setCorsHeaders = (req, res) => {
-//   const origin = req.headers.get('origin');
-
-//   // Check if the request's origin is in the allowed origins
-//   if (allowedOrigins.includes(origin)) {
-//     res.headers.set('Access-Control-Allow-Origin', origin);
-//   }
-
-//   res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-// };
 export async function POST(request) {
   const res = NextResponse.next(); 
   setCorsHeaders(res);
