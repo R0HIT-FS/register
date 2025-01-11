d9d6f84e6a2fd11c07bdf8f223bccc46a0719e19
R0hit-KP <rohit.yallamelli@kraftpixel.com>
2025-01-12 03:23:43 +0530

    Message changed

--- app/submitted/page.jsx
+++ app/submitted/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
         <div className='h-[250px] w-[250px] rounded-lg overflow-hidden mb-1'>
         <img className='h-full w-full object-cover object-left' src={success.src} alt="baby" /></div>
         <h1 className='text-green-500 text-xl md:text-3xl text-center'>Form Submitted.</h1>
-        <h3 className='text-white text-xl md:text-2xl mb-4 text-center'>We are pumped to see you there!</h3>
+        <h3 className='text-white text-xl md:text-2xl mb-4 text-center'>We are pumped to have you here!</h3>
         <p className='text-white text-xl md:text-2xl mt-2'>Location:</p>
         <iframe className='h-[400px] w-full md:w-[500px]' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3805.7303245927615!2d78.69873027493672!3d17.472616883429207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb765815870dad%3A0x11f0fea1b4dbb761!2sTENT%20-%20Carmel%20Campus!5e0!3m2!1sen!2sin!4v1735296873996!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
